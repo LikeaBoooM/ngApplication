8901b210a467e7babcf9bd5d803338aa88256127
LikeaBoooM <borkos44@gmail.com>
2021-12-15 15:24:51 +0100

    last changes

--- App/urls.py
+++ App/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('cars/<int:id>/', DeleteCar.as_view(), name='cars-ng-delete'),
-    path('rates/', RateCar.as_view(), name='cars-ng-rate-car'),
+    path('rate/', RateCar.as_view(), name='cars-ng-rate-car'),
     path('popular/', Popular.as_view(), name='cars-ng-popular'),
     path('cars/', CarsNG.as_view(), name='cars-ng-avg-rates'),
 ]
--- ngApplication/settings.py
+++ ngApplication/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%6f+isik^fpr!inj$jv5&27+*9%)nyw!nwe+w4vpzufy$%z&tl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
