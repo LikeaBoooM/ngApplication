73025c6d0fa366f4b6cb9ee5f335df0a59660ab8
LikeaBoooM <borkos44@gmail.com>
2021-12-15 21:10:38 +0100

    staticfiles reload

--- ngApplication/settings.py
+++ ngApplication/settings.py
@@ -138,7 +138,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Default primary key field type
