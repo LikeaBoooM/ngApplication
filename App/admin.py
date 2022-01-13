413ade56ef6e6d90df3a3c85e865ff15f25ae3ae
LikeaBoooM <borkos44@gmail.com>
2022-01-13 17:08:32 +0100

    read me important changes and views

--- App/admin.py
+++ App/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Car, Rate, Movie
+from .models import Car, Rate, Movie, Celcius
 # Register your models here.
 
 admin.site.register(Car)
 admin.site.register(Rate)
 admin.site.register(Movie)
+admin.site.register(Celcius)
